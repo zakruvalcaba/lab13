75d02d213b03491daa30fc58d670f131d090b78f
Zak Ruvalcaba <zakruvalcaba@gmail.com>
2023-11-16 13:34:59 -0800

    Updated code to add JSON.stringify and JSON.parse

--- js/script.js
+++ js/script.js
@@ -5,40 +5,41 @@ let task
 let tasks = []
 
 // GET THE DOM ELEMENTS
-const $ = (id) => {
-    return document.getElementById(id)
-}
+const $ = id => document.getElementById(id)
 
-// DISPLAY TASK LIST
-const displayTaskList = () => {
+// DISPLAY LIST OF TASKS
+function displayTaskList() {
     // IF THERE ARE NO TASKS IN ARRAY, CHECK STORAGE
     if (tasks.length === 0) {
-        // GET TASKS FROM STORAGE OR EMPTY STRING IF STORAGE IS EMPTY
+        // GET TASKS FROM STORAGE OR EMPTY STRING IF STORAGE IS ALSO EMPTY
         storage = localStorage.getItem('tasks') || ''
-        // IF NOT EMPTY, CONVERT TO ARRAY AND STORE IN TASKS VARIABLE
+        // IF NOT EMPTY, CONVERT STRING TO ARRAY AND STORE IN TASKS VARIABLE
         if (storage.length > 0) {
-            tasks = storage.split('|')
+            // tasks = storage.split('|') // WITHOUT JSON.PARSE
+            tasks = JSON.parse(localStorage.getItem('tasks'))
         }
     }
-    
+
     // IF THERE ARE TASKS IN THE ARRAY...
     // SORT AND CREATE TASKS STRING
+    // THEN DISPLAY STRING WITHIN TEXTAREA
     if (tasks.length > 0) {
-        tasks.sort()
-        list = tasks.join('\n')
+        tasks.sort()            // SORT
+        list = tasks.join('\n') // APPEND LINE BREAK
 
         // DISPLAY TASKS STRING
         $('task_list').value = list
     }
 }
 
-// ADD A TASK
-const addToTaskList = () => {
+// ADD TO TASK LIST
+function addToTaskList() {
     if ($('task').value !== '') {
         // ADD TASK TO ARRAY
         tasks.push($('task').value)
-        // ADD TASK TO STRORAGE
-        localStorage.setItem('tasks', tasks.join('|'))
+        // ADD TASK TO STORAGE
+        // localStorage.setItem('tasks', tasks.join('|')) // WITHOUT JSON.STRINGIFY
+        localStorage.setItem('tasks', JSON.stringify(tasks))
         // CLEAR THE TEXT BOX
         $('task').value = ''
         // SHOW UPDATED TASKS
@@ -49,15 +50,13 @@ const addToTaskList = () => {
 }
 
 // CLEAR TASK LIST
-const clearTaskList = () => {
+function clearTaskList() {
     tasks.length = 0                    // EMPTY ARRAY
     localStorage.removeItem('tasks')    // REMOVE TASKS FROM STORAGE
     $('task_list').value = ''           // CLEAR OUT TASK LIST FIELD
 }
 
-// WIRE UP EVENT HANDLERS AND DISPLAY ANY TASKS IN LIST
-window.addEventListener('load', () => {
-    $('add_task').addEventListener('click', addToTaskList)
-    $('clear_tasks').addEventListener('click', clearTaskList)
-    displayTaskList()
-})
+// WIRE UP EVENT HANDLERS AND DISPLAY TASKS IN LIST
+$('add_task').addEventListener('click', addToTaskList)
+$('clear_tasks').addEventListener('click', clearTaskList)
+displayTaskList()
